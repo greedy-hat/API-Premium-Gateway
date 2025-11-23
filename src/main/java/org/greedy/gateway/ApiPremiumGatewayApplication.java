6dfae12910685ab37bee36684a019b2c91ae2916
Wen <46677820+greedy-hat@users.noreply.github.com>
2025-11-23 15:14:35 +0800

    feat: 添加数据库表

---
+++ src/main/java/org/greedy/gateway/ApiPremiumGatewayApplication.java
@@ -0,0 +1,23 @@
+package org.greedy.gateway;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+/**
+ * API Premium Gateway 主启动类
+ * 
+ * @author greedy
+ * @since 1.0.0
+ */
+@SpringBootApplication(exclude = {
+    HibernateJpaAutoConfiguration.class
+})
+@MapperScan("org.xhy.gateway.domain.**.repository")
+public class ApiPremiumGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPremiumGatewayApplication.class, args);
+    }
+} 
